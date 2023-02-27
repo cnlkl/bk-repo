a757380d392158a13914e1b6f4024d748b19ca6b
yaoxuwan <yaoxuwan@tencent.com>
2023-02-27 12:04:58 +0800

    feat: 增加EdgePlus集群角色 #224

--- src/backend/common/common-api/src/main/kotlin/com/tencent/bkrepo/common/api/constant/Constants.kt
+++ src/backend/common/common-api/src/main/kotlin/com/tencent/bkrepo/common/api/constant/Constants.kt
@@ -86,3 +86,18 @@ const val ANALYSIS_EXECUTOR_SERVICE_NAME = "\${service.prefix:}analysis-executor
 const val HELM_SERVICE_NAME = "\${service.prefix:}helm\${service.suffix:}"
 const val OCI_SERVICE_NAME = "\${service.prefix:}docker\${service.suffix:}"
 const val JOB_SERVICE_NAME = "\${service.prefix:}job\${service.suffix:}"
+
+/**
+ * 认证相关
+ */
+const val BASIC_AUTH_PREFIX = "Basic "
+const val BASIC_AUTH_PROMPT = "Basic realm=\"Authentication Required\""
+const val PLATFORM_AUTH_PREFIX = "Platform "
+const val BEARER_AUTH_PREFIX = "Bearer "
+const val AUTH_HEADER_UID = "X-BKREPO-UID"
+const val OAUTH_AUTH_PREFIX = "Oauth"
+
+/**
+ * micro service header user id key
+ */
+const val MS_AUTH_HEADER_UID = "X-BKREPO-MS-UID"
