a8bbc19fa6d6654d0ab9224e03b31448ee6ff523
zacYL <zacyanliu@tencent.com>
2023-03-27 22:56:33 +0800

    bugfix: 写超时时间调整 TencentBlueKing#301

--- src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/context/ReplicaContext.kt
+++ src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/context/ReplicaContext.kt
@@ -180,6 +180,6 @@ class ReplicaContext(
     companion object {
         private val logger = LoggerFactory.getLogger(ReplicaContext::class.java)
         private const val READ_TIMEOUT = 60 * 60 * 1000L
-        private const val WRITE_TIMEOUT = 30 * 1000L
+        private const val WRITE_TIMEOUT = 5 * 1000L
     }
 }
