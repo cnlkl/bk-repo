7000ca2e4919708fad93ad18aabe61c7f0c2f225
kunlongli <kunlongli@tencent.com>
2023-03-31 13:30:51 +0800

    feat: 增加socket write与close日志 #301

--- src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/interceptor/SocketInterceptor.kt
+++ src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/interceptor/SocketInterceptor.kt
@@ -27,18 +27,119 @@
 
 package com.tencent.bkrepo.replication.replica.base.interceptor
 
+import com.tencent.bkrepo.replication.pojo.task.ReplicaTaskInfo
+import com.tencent.bkrepo.replication.replica.base.interceptor.progress.ProgressRequestBody
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
+import okhttp3.internal.connection.RealConnection
+import okio.Buffer
+import okio.Sink
+import okio.Timeout
 import org.slf4j.LoggerFactory
+import java.lang.reflect.Field
+import java.net.Socket
 
 class SocketInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
+        val url = chain.request().url
+        if (url.encodedPath.contains("/replica/blob/push")) {
+            // 为push blob接口调用添加socket关闭过程日志
+            proxySocket(chain.connection() as RealConnection, chain.request())
+        }
+
         logger.info("SocketInterceptor connection ${chain.connection()}")
         logger.info("SocketInterceptor before set so_linger ${chain.connection()?.socket()?.keepAlive}")
         return chain.proceed(chain.request())
     }
 
+    private fun proxySocket(connection: RealConnection, request: Request) {
+        val body = request.body as ProgressRequestBody
+        val task = getField(ProgressRequestBody::class.java.getDeclaredField("task"), body) as ReplicaTaskInfo
+        val pushingBlobSha256 =
+            getField(ProgressRequestBody::class.java.getDeclaredField("sha256"), body).toString()
+
+        val bufferSink = getField(RealConnection::class.java.getDeclaredField("sink"), connection)
+        val timeoutSinkField = bufferSink.javaClass.getDeclaredField("sink")
+        val timeoutSink = getField(timeoutSinkField, bufferSink)
+        // okio.AsyncTimeout.sink中返回的匿名内部类对象，第一个field为timeout,第二个为实际sink
+        val asyncTimeout = getField(timeoutSink.javaClass.declaredFields[0], timeoutSink)
+        val outputStreamSinkField = timeoutSink.javaClass.declaredFields[1]
+        val outputStreamSink = getField(outputStreamSinkField, timeoutSink)
+
+        // proxy socket
+        val asyncTimeoutSocketField = asyncTimeout.javaClass.getDeclaredField("socket")
+        val realSocket = getField(asyncTimeoutSocketField, asyncTimeout)
+        asyncTimeoutSocketField.set(asyncTimeout, ProxySocket(task, pushingBlobSha256, realSocket as Socket))
+
+
+        // proxy sink
+        outputStreamSinkField.set(timeoutSink, ProxySink(task, pushingBlobSha256, outputStreamSink as Sink))
+    }
+
+    private fun getField(field: Field, obj: Any): Any {
+        field.isAccessible = true
+        return field.get(obj)
+    }
+
+    private class ProxySink(
+        private val taskInfo: ReplicaTaskInfo,
+        private val sha256: String,
+        private val proxiedSink: Sink
+    ) : Sink {
+        override fun close() {
+            proxiedSink.close()
+        }
+
+        override fun flush() {
+            logger.info("push blob[$sha256], flushing sink, task[$taskInfo]")
+            try {
+                proxiedSink.flush()
+            } catch (e: Exception) {
+                logger.info("push blob[$sha256], flush sink failed, task[$taskInfo]")
+                e.printStackTrace()
+                throw e
+            }
+            logger.info("push blob[$sha256], flush sink success, task[$taskInfo]")
+        }
+
+        override fun timeout(): Timeout {
+            return proxiedSink.timeout()
+        }
+
+        override fun write(source: Buffer, byteCount: Long) {
+            logger.info("push blob[$sha256], writing $byteCount byte to sink, task[$taskInfo]")
+            try {
+                proxiedSink.write(source, byteCount)
+            } catch (e: Exception) {
+                logger.info("push blob[$sha256], write $byteCount byte to sink failed, task[$taskInfo]")
+                e.printStackTrace()
+                throw e
+            }
+            logger.info("push blob[$sha256], write $byteCount byte to sink success, task[$taskInfo]")
+        }
+
+    }
+
+    private class ProxySocket(
+        private val taskInfo: ReplicaTaskInfo,
+        private val sha256: String,
+        private val proxiedSocket: Socket
+    ) : Socket() {
+        // SocketAsyncTimeout中只会调用close方法，仅进行测试，所以只需要代理close方法就行
+        override fun close() {
+            logger.info("closing socket of push file[$sha256], task[$taskInfo]")
+            try {
+                proxiedSocket.close()
+            } catch (e: Exception) {
+                logger.info("close socket of push file[$sha256] failed, task[$taskInfo]")
+                e.printStackTrace()
+                throw e
+            }
+            logger.info("close socket of push file[$sha256] finished, task[$taskInfo]")
+        }
+    }
 
     companion object {
         private val logger = LoggerFactory.getLogger(SocketInterceptor::class.java)
